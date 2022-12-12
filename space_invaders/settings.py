dd80f5f05951363b09ccb9eae31cdfa54fc48765
TheArnoldPickaxe <thearnoldpickaxe@gmail.com>
2022-12-12 14:43:54 +0800

    cleaning some stuff up and actually sane difficulty progression

--- space_invaders/settings.py
+++ space_invaders/settings.py
@@ -14,15 +14,15 @@ class Settings:
         self.bullets_allowed = 5
 
         self.fleet_drop_speed = 10
-        self.speedup_scale = 1.1
+        self.speedup_scale = 1.2
         self.score_scale = 1.5
 
         self.initialize_dynamic_settings()
 
     def initialize_dynamic_settings(self):
-        self.ship_speed = 1.5
-        self.bullet_speed = 1.5
-        self.alien_speed = 1.0
+        self.ship_speed = 2.0
+        self.bullet_speed = 2.0
+        self.alien_speed = 2.0
 
         self.fleet_direction = 1 #1 right -1 left
 
